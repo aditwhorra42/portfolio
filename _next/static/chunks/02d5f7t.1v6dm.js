3bbbdef92638ea301412b45c69e99804e0fbfbb1
aditwhorra42 <aditwhorra42@users.noreply.github.com>
2026-03-23 11:34:23 +0000

    deploy: a230e66bea2c9aafa1023ba83b8c57d25faf18e8

---
+++ _next/static/chunks/02d5f7t.1v6dm.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,47297,t=>{"use strict";var c=t.i(71645);t.s(["default",0,function(){return(0,c.useEffect)(()=>{window.location.replace("/cv/adit-whorra-cv.pdf")},[]),null}])}]);
